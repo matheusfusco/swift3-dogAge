e9c01bb0350605bdd4c3f65f4e43abf495a2f564
matheusfusco <matheus.fusco@conductor.com.br>
2016-12-02 10:51:48 -0200

    Initial Commit

---
+++ Idade de Cachorro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Idade de Cachorro
+//
+//  Created by Matheus Fusco on 02/12/16.
+//  Copyright © 2016 Matheus Fusco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
